96ef02b4d3000d6c2c5e89e47fcab55a95d6ee26
BlackFeather <she3354248@yahoo.com.tw>
2017-02-13 17:55:55 +0800

    add test connection.js

---
+++ test/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+
+mongoose.connect('mongodb://localhost/testaroo');
+mongoose.connection.once('open', function(){
+  console.log('Connection has been made, now make fireworks...');
+}).on('error', function(error){
+  console.log('Connection error:', error);
+});
