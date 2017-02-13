090f66ddac24585067f5469dc6e51e755a8b85fa
BlackFeather <she3354248@yahoo.com.tw>
2017-02-13 22:27:43 +0800

    add a book to author form nesting_test

--- test/nesting_test.js
+++ test/nesting_test.js
@@ -4,6 +4,12 @@ const Author = require('../models/author');
 
 describe('Nesting records', function(){
 
+  beforeEach(function(done){
+    mongoose.connection.collections.authors.drop(function(){
+      done();
+    });
+  });
+
   it('Create an author with sub-documents', function(done){
 
     var pat = new Author({
@@ -20,6 +26,33 @@ describe('Nesting records', function(){
         done();
       });
     })
+
+  });
+
+  it('Adds to book to an author', function(done){
+
+    var pat = new Author({
+      name: 'Patrick Rothfuss',
+      books: [{
+        title: 'Name of the Wind',
+        pages: 400
+      }]
+    });
+
+    pat.save().then(function(){
+      Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+        record.books.push({
+          title: "Wise Man's Fear",
+          pages: 500
+        });
+        record.save().then(function(){
+          Author.findOne({name: 'Patrick Rothfuss'}).then(function(result){
+            assert(result.books.length === 2);
+            done();
+          });
+        })
+      });
+    });
 
   });
 
