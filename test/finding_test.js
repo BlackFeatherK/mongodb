27c5ec51a10a48d16e320685f264f6e6c0c14819
BlackFeather <she3354248@yahoo.com.tw>
2017-02-13 20:16:29 +0800

    add find by ID to finding_test

--- test/finding_test.js
+++ test/finding_test.js
@@ -3,9 +3,11 @@ const MarioChar = require('../models/mariochar');
 
 describe('Finding records', function(){
 
+  var char;
+
   beforeEach(function(done){
 
-    var char = new MarioChar({
+    char = new MarioChar({
       name: 'Mario'
     });
 
@@ -19,6 +21,13 @@ describe('Finding records', function(){
   it('Finds one record to the database', function(done){
     MarioChar.findOne({name: 'Mario'}).then(function(result){
       assert(result.name === 'Mario');
+      done();
+    });
+  });
+
+  it('Finds one record by ID to the database', function(done){
+    MarioChar.findOne({_id: char._id}).then(function(result){
+      assert(result._id.toString() === char._id.toString());
       done();
     });
   });
